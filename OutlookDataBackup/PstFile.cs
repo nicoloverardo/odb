061e3ef34ef6546212629483b375f31b349577c1
Nicolò Verardo <nicoloverardo@users.noreply.github.com>
2020-03-04 01:08:45 +0100

    Add files via upload

---
+++ OutlookDataBackup/PstFile.cs
@@ -0,0 +1,123 @@
+﻿namespace OutlookDataBackup
+{
+    public class PstFile
+    {
+        /// <summary>
+        /// Represents a PST or OST file.
+        /// </summary>
+        /// <param name="name">File name, including extension</param>
+        /// <param name="path">File full local path</param>
+        /// <param name="destination">File full online path (i.e: /Documents/Backup/myfile.pst)</param>
+        /// <param name="length">File length as long.</param>
+        public PstFile(string name, string path, string destination, long length)
+        {
+            Name = name;
+            Path = path;
+            Destination = destination;
+            Length = length;
+            Size = Helpers.GetReadableSize(Length);
+            Progress = 0;
+            NeedsZip = SizeExceedsLimit(Length);
+        }
+
+        /// <summary>
+        /// Represents a PST or OST file.
+        /// </summary>
+        /// <param name="name">File name, including extension</param>
+        /// <param name="path">File full local path</param>
+        /// <param name="destination">File full online path (i.e: /Documents/Backup/myfile.pst)</param>
+        /// <param name="length">File length as long.</param>
+        /// <param name="progress">Indicates the current upload progress of the file</param>
+        public PstFile(string name, string path, string destination, long length, double progress)
+        {
+            Name = name;
+            Path = path;
+            Destination = destination;
+            Length = length;
+            Size = Helpers.GetReadableSize(Length);
+            Progress = progress;
+            NeedsZip = SizeExceedsLimit(Length);
+        }
+
+        /// <summary>
+        /// Represents a PST or OST file.
+        /// </summary>
+        /// <param name="name">File name, including extension</param>
+        /// <param name="path">File full local path</param>
+        /// <param name="destination">File full online path (i.e: /Documents/Backup/myfile.pst)</param>
+        /// <param name="length">File length as long.</param>
+        /// <param name="progress">Indicates the current upload progress of the file</param>
+        /// <param name="hash">Hash value</param>
+        public PstFile(string name, string path, string destination, long length, double progress, int hash)
+        {
+            Name = name;
+            Path = path;
+            Destination = destination;
+            Length = length;
+            Size = Helpers.GetReadableSize(Length);
+            Progress = progress;
+            NeedsZip = SizeExceedsLimit(Length);
+            Hash = hash;
+        }
+
+        /// <summary>
+        /// Gets the file name, including extension.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets the file full local path.
+        /// </summary>
+        public string Path { get; }
+
+        /// <summary>
+        /// Gets the file full online path.
+        /// </summary>
+        public string Destination { get; }
+
+        /// <summary>
+        /// Gets the file length as long.
+        /// </summary>
+        public long Length { get; }
+
+        /// <summary>
+        /// Gets or sets the file upload progress.
+        /// </summary>
+        public double Progress { get; set; }
+
+        /// <summary>
+        /// Gets the file human-readable size.
+        /// </summary>
+        public string Size { get; }
+
+        /// <summary>
+        /// Indicates if the file size exceeds the OneDrive upload limit and therefore it needs zipping.
+        /// </summary>
+        public bool NeedsZip { get; }
+
+        /// <summary>
+        /// Gets or sets the Hash value
+        /// </summary>
+        public int Hash { get; set; }
+
+        /// <summary>
+        /// Check if file size exceeds the OneDrive upload limit for a single file.
+        /// </summary>
+        /// <param name="length">File length as long.</param>
+        /// <returns>True if it exceeds the limit, otherwise false</returns>
+        private static bool SizeExceedsLimit(long length)
+        {
+            long absolute = length < 0 ? -length : length;
+
+            if (absolute >= 0x40000000) // Gigabyte
+            {
+                double readable = (length >> 20);
+                readable = (readable / 1024);
+
+                return readable >= 10;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ OutlookDataBackup/SettingsHandler.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
+namespace OutlookDataBackup
+{
+    public class SettingsHandler
+    {
+        private string SettingsFolder { get; set; } 
+        private string ConfigFilePath { get; set; }
+        public string Token { get; set; }
+        public string ConflictBehavior { get; set; }
+        public string SplitSize { get; set; }
+        public Dictionary<PstFile, List<PstFile>> Files { get; set; }
+
+        public SettingsHandler(string settingsFolder)
+        {
+            if (!Directory.Exists(settingsFolder))
+            {
+                Directory.CreateDirectory(settingsFolder);
+            }
+
+            SettingsFolder = settingsFolder;
+            ConfigFilePath = SettingsFolder + @"\settings";
+
+            if (!File.Exists(settingsFolder + @"\settings"))
+            {
+                CreateXml();
+            }     
+
+            Load();
+        }
+
+        private void CreateXml()
+        {
+            using (XmlWriter writer = XmlWriter.Create(ConfigFilePath))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Settings");
+                writer.WriteElementString("Token", "");
+                writer.WriteElementString("ConflictBehavior", "0");
+                writer.WriteElementString("SplitSize", "-v2g");
+                writer.WriteStartElement("Files");
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+            }
+        }
+
+        public void Load()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(ConfigFilePath);
+
+            Token = Unprotect(xmlDoc.DocumentElement.SelectSingleNode("/Settings/Token").Value); // OR INNERTEXT?
+            ConflictBehavior = xmlDoc.DocumentElement.SelectSingleNode("/Settings/ConflictBehavior").Value == "0" ?
+                "replace" :
+                "rename";
+            SplitSize = xmlDoc.DocumentElement.SelectSingleNode("/Settings/SplitSize").Value;
+            Files = ReadFilesSetting(xmlDoc);
+
+            xmlDoc.Save(ConfigFilePath);
+            xmlDoc = null;
+            GC.Collect();
+        }
+
+        public Dictionary<PstFile, List<PstFile>> ReadFilesSetting(XmlDocument xmlDoc = null)
+        {
+            if (xmlDoc == null)
+            {
+                xmlDoc = new XmlDocument();
+                xmlDoc.Load(ConfigFilePath);
+            }
+
+            XmlNode files = xmlDoc.DocumentElement.SelectSingleNode("/Settings/Files");
+            Dictionary<PstFile, List<PstFile>> finalList = new Dictionary<PstFile, List<PstFile>>();
+
+            if (files.ChildNodes.Count == 0) return finalList;
+
+            foreach (XmlNode file in files.ChildNodes)
+            {
+                string name = file.Attributes["name"].Value;
+                string location = file.ChildNodes.Item(0).Value;
+                string destination = file.ChildNodes.Item(1).Value;
+                long size = Convert.ToInt64(file.ChildNodes.Item(2).Value);
+                double progress = Convert.ToDouble(file.ChildNodes.Item(3).Value);
+
+                List<PstFile> zipParts = new List<PstFile>();
+                if (file.LastChild.Name == "ZipParts")
+                {
+                    foreach (XmlNode part in file.LastChild.ChildNodes)
+                    {
+                        string partName = part.Attributes["name"].Value;
+                        string partLocation = part.ChildNodes.Item(0).Value;
+                        string partDestination = file.ChildNodes.Item(1).Value;
+                        long partSize = Convert.ToInt64(file.ChildNodes.Item(2).Value);
+                        double partProgress = Convert.ToDouble(file.ChildNodes.Item(3).Value);
+                        int partHash = Convert.ToInt32(file.ChildNodes.Item(4).Value);
+
+                        zipParts.Add(new PstFile(partName, partLocation, partDestination, partSize, partProgress, partHash));
+                    }
+                }
+
+                finalList.Add(new PstFile(name, location, destination, size, progress), zipParts);
+            }
+
+            return finalList;
+        }
+
+        public void SaveFilesSetting(XmlDocument xmlDoc = null)
+        {
+            if (xmlDoc == null)
+            {
+                xmlDoc = new XmlDocument();
+                xmlDoc.Load(ConfigFilePath);
+            }
+
+            XmlNode filesNode = xmlDoc.DocumentElement.SelectSingleNode("/Settings/Files");
+            foreach (var item in Files)
+            {
+                XmlElement itemNode = xmlDoc.CreateElement("File");
+                XmlAttribute attribute = xmlDoc.CreateAttribute("name");
+                attribute.Value = item.Key.Name;
+                itemNode.Attributes.Append(attribute);
+
+                XmlElement locationNode = xmlDoc.CreateElement("Location");
+                locationNode.Value = item.Key.Path;
+                itemNode.AppendChild(locationNode);
+
+                XmlElement destinationNode = xmlDoc.CreateElement("Destination");
+                destinationNode.Value = item.Key.Destination;
+                itemNode.AppendChild(destinationNode);
+
+                XmlElement sizeNode = xmlDoc.CreateElement("Size");
+                sizeNode.Value = item.Key.Length.ToString();
+                itemNode.AppendChild(sizeNode);
+
+                XmlElement progressNode = xmlDoc.CreateElement("Progress");
+                progressNode.Value = item.Key.Progress.ToString();
+                itemNode.AppendChild(progressNode);
+
+                if (item.Value.Count != 0)
+                {
+                    XmlElement parts = xmlDoc.CreateElement("ZipParts");
+                    foreach (var part in item.Value)
+                    {
+                        XmlElement partNode = xmlDoc.CreateElement("Part");
+                        XmlAttribute partAttribute = xmlDoc.CreateAttribute("name");
+                        partAttribute.Value = part.Name;
+                        partNode.Attributes.Append(partAttribute);
+
+                        XmlElement partLocationNode = xmlDoc.CreateElement("Location");
+                        partLocationNode.Value = part.Path;
+                        partNode.AppendChild(partLocationNode);
+
+                        XmlElement partDestinationNode = xmlDoc.CreateElement("Destination");
+                        partDestinationNode.Value = part.Destination;
+                        partNode.AppendChild(partDestinationNode);
+
+                        XmlElement partSizeNode = xmlDoc.CreateElement("Size");
+                        partSizeNode.Value = part.Length.ToString();
+                        partNode.AppendChild(partSizeNode);
+
+                        XmlElement partProgressNode = xmlDoc.CreateElement("Progress");
+                        partProgressNode.Value = part.Progress.ToString();
+                        partNode.AppendChild(partProgressNode);
+
+                        XmlElement partHashNode = xmlDoc.CreateElement("Hash");
+                        partHashNode.Value = (new FileInfo(part.Path)).GetHashCode().ToString();
+                        partNode.AppendChild(partHashNode);
+
+                        parts.AppendChild(partNode);
+                    }
+
+                    itemNode.AppendChild(parts);
+                }
+
+                filesNode.AppendChild(itemNode);
+            }
+
+            xmlDoc.Save(ConfigFilePath);
+        }
+
+        public void Clear(XmlDocument xmlDoc = null)
+        {
+            if (xmlDoc == null)
+            {
+                xmlDoc = new XmlDocument();
+                xmlDoc.Load(ConfigFilePath);
+            }
+
+            xmlDoc.DocumentElement.SelectSingleNode("/Settings/Token").Value = "";
+            xmlDoc.DocumentElement.SelectSingleNode("/Settings/ConflictBehavior").Value = "0";
+            xmlDoc.DocumentElement.SelectSingleNode("/Settings/SplitSize").Value = "";
+            xmlDoc.DocumentElement.SelectSingleNode("/Settings/Files").RemoveAll();
+
+            xmlDoc.Save(ConfigFilePath);
+            xmlDoc = null;
+            GC.Collect();
+        }
+
+        private string LoadSingleSetting(string setting, XmlDocument xmlDoc = null)
+        {
+            if (xmlDoc == null)
+            {
+                xmlDoc = new XmlDocument();
+                xmlDoc.Load(ConfigFilePath);
+            }
+
+            string result = xmlDoc.DocumentElement.SelectSingleNode("/Settings/" + setting).Value;
+
+            xmlDoc.Save(ConfigFilePath);
+            xmlDoc = null;
+
+            return result;
+        }
+
+        private void SaveSingleSetting(string setting, string value, XmlDocument xmlDoc = null)
+        {
+            if (xmlDoc == null)
+            {
+                xmlDoc = new XmlDocument();
+                xmlDoc.Load(ConfigFilePath);
+            }
+
+            xmlDoc.DocumentElement.SelectSingleNode("/Settings/" + setting).Value = value;
+
+            xmlDoc.Save(ConfigFilePath);
+            xmlDoc = null;
+            GC.Collect();
+        }
+
+        public void Save()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(ConfigFilePath);
+
+            xmlDoc.DocumentElement.SelectSingleNode("/Settings/Token").Value = Protect(Token);
+            xmlDoc.DocumentElement.SelectSingleNode("/Settings/ConflictBehavior").Value = ConflictBehavior == "0" ?
+                "replace" :
+                "rename";
+
+            xmlDoc.DocumentElement.SelectSingleNode("/Settings/SplitSize").Value = SplitSize;
+            SaveFilesSetting(xmlDoc);
+
+            xmlDoc.Save(ConfigFilePath);
+            xmlDoc = null;
+            GC.Collect();
+        }
+
+        private static string Protect(string str)
+        {
+            byte[] entropy = Encoding.UTF8.GetBytes(Assembly.GetExecutingAssembly().FullName);
+            byte[] data = Encoding.UTF8.GetBytes(str);
+            string protectedData = Convert.ToBase64String(ProtectedData.Protect(data, entropy, DataProtectionScope.CurrentUser));
+            return protectedData;
+        }
+
+        private static string Unprotect(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return "";
+
+            byte[] protectedData = Convert.FromBase64String(str);
+            byte[] entropy = Encoding.UTF8.GetBytes(Assembly.GetExecutingAssembly().FullName);
+            string data = Encoding.UTF8.GetString(ProtectedData.Unprotect(protectedData, entropy, DataProtectionScope.CurrentUser));
+            return data;
+        }
+    }
+}
